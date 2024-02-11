51280077c3865f6f401ffd4a2b820a31cbb653d1
Олександр <aleksandrlulenko@gmail.com>
2024-02-11 19:51:35 +0100

    x

---
+++ src/js/1-timer.js
@@ -0,0 +1,83 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const inputEl = document.querySelector('#datetime-picker');
+const buttonEl = document.querySelector('[data-start]');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
+let userSelectedDate;
+buttonEl.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    userSelectedDate = selectedDates[0];
+    if (userSelectedDate <= new Date().getTime()) {
+      buttonEl.disabled = true;
+      iziToast.show({
+        title: '❌',
+        message: 'Please choose a date in the future',
+        messageColor: '#fff',
+        messageSize: '16px',
+        position: 'topRight',
+        backgroundColor: '#EF4040',
+        close: false,
+        closeOnClick: true,
+      });
+    } else {
+      buttonEl.disabled = false;
+    }
+  },
+};
+
+flatpickr(inputEl, options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+buttonEl.addEventListener('click', onStartTimer);
+
+function onStartTimer() {
+  buttonEl.disabled = true;
+  inputEl.disabled = true;
+  const interval = setInterval(() => {
+    const diff = userSelectedDate - Date.now();
+    if (diff <= 1000) {
+      clearInterval(interval);
+      inputEl.disabled = false;
+    }
+    const time = convertMs(diff);
+    daysEl.textContent = addLeadingZero(time.days);
+    hoursEl.textContent = addLeadingZero(time.hours);
+    minutesEl.textContent = addLeadingZero(time.minutes);
+    secondsEl.textContent = addLeadingZero(time.seconds);
+  }, 1000);
+}
